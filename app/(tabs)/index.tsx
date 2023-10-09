200ab0d6a5b9cc3213959e98f028b33c8d9321c9
Atanas Kanchev <atanas.kanchev@valens-services.co.uk>
2023-10-09 09:53:21 +0100

    repro

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { Text as RNText, View as RNView, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+
+      <RNView className="h-25 w-25 bg-red-500">
+        <RNText className="color-red-500">NW alpha 15</RNText>
+      </RNView>
+
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +25,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
@@ -7,4 +7,5 @@ const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
 });
 
-module.exports = config;
+const { withNativeWind } = require("nativewind/metro");
+module.exports = withNativeWind(config, { input: "global.css" });
